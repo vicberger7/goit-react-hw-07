53826bc9497b5c0a273fa65d654c36d884d1226c
Viktor Atsberher <143943277+vicberger7@users.noreply.github.com>
2024-04-19 18:47:19 +0200

    hw7-redux-ft

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,17 @@ import ContactList from "./ContactList/ContactList";
 import ContactForm from "./ContactForm/ContactForm";
 import SearchBox from "./SearchBox/SearchBox";
 import css from "./App.module.css";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchContacts } from "../../redux/contactsSlice";
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <div className={css.container}>
       <h1>Phonebook</h1>
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,17 +1,16 @@
 import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
 import { useDispatch, useSelector } from "react-redux";
-import { deleteContact, selectContacts } from "../../redux/contactsSlice";
-import { selectNameFilter } from "../../redux/filtersSlice";
+import { deleteContact } from "../../redux/contactsSlice";
+// import { selectNameFilter } from "../../redux/filtersSlice";
+import { selectFilteredContacts } from "../../redux/contactsSlice";
 
 export default function ContactList() {
   const dispatch = useDispatch();
-  const search = useSelector(selectNameFilter);
-  const contacts = useSelector(selectContacts);
+  // const search = useSelector(selectNameFilter);
+  // const contacts = useSelector(selectContacts);
   const onDeleteContact = (id) => dispatch(deleteContact(id));
-  const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(search.toLowerCase())
-  );
+  const filteredContacts = useSelector(selectFilteredContacts);
 
   return (
     <ul className={css.list}>
